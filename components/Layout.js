6ccf7bad09a0f38498af8d4554d1225a7a7503cf
chomeko3o <kusyopi@gmail.com>
2020-03-10 21:12:51 +0900

    head情報追加

--- components/Layout.js
+++ components/Layout.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import style from '../static/Style';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Head from 'next/head';
 
 class Layout extends Component {
   render() {
     return (
       <div>
+        <Head>
+          <title>{this.props.title}</title>
+          <meta charSet='uft-8' />
+          <meta name='viewport'
+            content='initial-scale=1.0, width=device-width' />
+        </Head>
         {style}
         <Header
           header={this.props.header}
